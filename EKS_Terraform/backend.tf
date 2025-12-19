2968a06e36e0cc80c9ad4d1496121f4ed6ab3a6d
RoseTechWorks <Naomiobebeduo@gmail.com>
2025-12-19 14:47:51 -0600

    Update S3 bucket name for Terraform backend

--- EKS_Terraform/backend.tf
+++ EKS_Terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "happy-docker-tfstate" # Replace with your actual S3 bucket name
+    bucket = "happy-docker-tfstate-v2" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "us-east-2"
   }
